0a3ab2cbea02fe5d21afd17c89d31e107c0f8b24
Jonathan Baker <Jonathantuan02@gmail.com>
2018-04-03 10:11:18 -0700

    added New customer view and actionresult New() in customer Controller

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -42,6 +42,10 @@ namespace Vidly.Controllers
             return View(customer);
         }
 
+        public ActionResult New()
+        {
+            return View();
+        }
         //private IEnumerable<Customer> GetCustomers()
         //{
         //    return new List<Customer>
